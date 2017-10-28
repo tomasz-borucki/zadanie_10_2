eec848cde7ed653608a7845f06d767bc97c4d4cf
Your Name <you@example.com>
2017-10-28 23:23:46 +0200

    Add files to project

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$("span:even").css('color', 'red');
+var paragraphs = $('p');
+paragraphs.each(function(index, element) {
+    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+    $(element).append(button)
+});
+$("button").click(function(){
+	alert($(this).attr("data-tmp"));
+});
